502fd39b63f8dd59de705720554e7b6ae7d1f32b
undefinedzack <adhney.nawghare@gmail.com>
2020-11-16 13:52:19 +0530

    adding logo

--- frontend/src/Components/navbar.js
+++ frontend/src/Components/navbar.js
@@ -5,7 +5,14 @@ class NavigationBar extends Component{
     render() {
         return(
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <img src="" width="30" height="30"
+                     className="d-inline-block align-top" alt="" loading="lazy"/>
                 <Link to={'/'}><div className="navbar-brand">.project</div></Link>
+                <button className="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
 
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto">
